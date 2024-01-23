61cd3f90cdb23e1ff510120fc39a3c2b15d88b87
amisha26 <amisha8299@gmail.com>
2024-01-23 17:55:45 -0500

    addeed csv data

--- server/server.py
+++ server/server.py
@@ -2,182 +2,184 @@ import os
 from pathlib import Path
 from flask_cors import CORS  # Import CORS from Flask-CORS
 from flask import Flask, jsonify, request, send_from_directory
+import csv
+
 
 app = Flask(__name__)
 CORS(app)
 
-historyData = [
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 6},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 1},
-            {"x": 20, "y": 9},
-        ],
-    },
-    {
-        "id": "Metric History",
-        "data": [
-            {"x": -20, "y": 1},
-            {"x": -10, "y": 7},
-            {"x": 0, "y": 2},
-            {"x": 10, "y": 6},
-            {"x": 20, "y": 5},
-        ],
-    },
-]
-speedData = [0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1]
-boomData = speedData[::-1]
-gpaData = [1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1]
+# historyData = [
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 6},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 1},
+#             {"x": 20, "y": 9},
+#         ],
+#     },
+#     {
+#         "id": "Metric History",
+#         "data": [
+#             {"x": -20, "y": 1},
+#             {"x": -10, "y": 7},
+#             {"x": 0, "y": 2},
+#             {"x": 10, "y": 6},
+#             {"x": 20, "y": 5},
+#         ],
+#     },
+# ]
+# speedData = [0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1]
+# boomData = speedData[::-1]
+# gpaData = [1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1]
 
 
 # Define the absolute path to the 'client/build' directory
 buildFilePath = Path(__file__).resolve().parent.parent / 'client' / 'build'
 print(buildFilePath, ": path")
 
-
+csvFilePath = Path(__file__).resolve().parent / 'utils' / 'data_csv.csv'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -194,9 +196,44 @@ def serve(path):
 @app.route('/history-data', methods=['GET'])
 def hello():
     time = int(request.args.get('time'))
-    if time >= 15:
+    if time >= 260:
         time = 0
-    return jsonify({'historyData': [historyData[time]], "gpaData": gpaData[time], "boomData": boomData[time], "speedData": speedData[time]}), 200
+
+    # Read CSV data
+    with open(csvFilePath, newline='') as csvFile:
+        reader = csv.DictReader(csvFile)
+        history_data = []
+
+        for _ in range(time + 1):
+            row = next(reader, None)
+
+        if row:
+            # Initialize data list if it doesn't exist
+            if 'historyData' not in row:
+                row['historyData'] = []
+
+            # Append the current data point
+            row['historyData'].append({
+                "id": "Metric History",
+                "data": [
+                    {
+                        "x": float(row["time"]),
+                        "y": float(row["metric"])
+                    },
+                ]
+            })
+
+            gpaData = int(row["gpa"])
+            boomData = int(row["boom"])
+            speedData = int(row["speed"])
+
+            # Return historyData as an array
+            return jsonify({'historyData': row['historyData'], "gpaData": gpaData, "boomData": boomData, "speedData": speedData}), 200
+
+        else:
+            return jsonify({'error': 'Invalid params'})
+
+
 
 
 if __name__ == '__main__':
